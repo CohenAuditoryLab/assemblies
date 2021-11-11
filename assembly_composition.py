97ba1cc84c84947e5ea8adaa00da3ff1eb5a878d
jylls <jean-hugues.lestang@pennmedicine.upenn.edu>
2021-11-11 15:11:47 -0500

    Update assembly_composition.py

--- assembly_composition.py
+++ assembly_composition.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 def assembly_composition(rot_seg,sd,m):
-
+ 
     for v in range(rot_seg.shape[0]):
         for w in range(rot_seg.shape[1]):
-            if rot_seg[v,w]>=(m-1.5*sd) and rot_seg[v,w]<=(m+1.5*sd):
-                rot_seg[v,w]=0
-
+            if rot_seg[v,w]>=m+0.5*sd or rot_seg[v,w]<=-m-0.5*sd:
+                rot_seg[v,w]=1
+            else:
+              rot_seg[v,w]=0
     return rot_seg
